25e01a61a86ff9b77db80ed7af6054120a6e1b46
Liz Westberg <ewest081@gmail.com>
2015-12-20 12:32:44 -0600

    add functionality and style to buttons

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,23 +1,16 @@
+var nextArrow = '\u2192';
+var prevArrow = '\u2190';
+
 $(document).ready(function(){
     getData();
     buttonMaker();
 
-    //first sad attempt at a carousel effect... with interesting results.
-    var t = setInterval(function(){
+    $('.button-holder').on('click', '.previous', clickPrevious);
+    $('.button-holder').on('click', '.next', clickNext);
 
-        //$("#container ul").animate({marginLeft:-480},1000,function(){
-        //    $(this).find("li:last").after($(this).find("li:first"));
-        //    $(this).css({marginLeft:0});
-        //})
-
-        $('#container ul').fadeIn(400,function(){
-            $(this).find("li:last").after($(this).find("li:first"))
-        })
-
-    },10000);
+    intervalTimer();
 
 });
-
 
 //get data function we were given.
 function getData(){
@@ -28,27 +21,68 @@ function getData(){
         url:"/data",
         success: function(people){
             data = people;
-            console.log(data.people.length);
             logData(data);
         }
     });
 }
 
-//next/prev button creator in the doc.ready?
-//we'll see if this is necessary or if it's included in the carousel code below... somehow I doubt the latter.
+//next/prev button creator in the doc.ready
 function buttonMaker(){
-    $('.button-holder').append('<button class="previous">Previous</button>');
-    $('.button-holder').append('<button class="next">Next</button>');
+    $('.button-holder').append('<button class="previous">' + prevArrow + '</button>');
+    $('.button-holder').append('<button class="next">' + nextArrow + '</button>');
 }
-
-
 
 //Function to append dom with divs containing the theta info from data.people. Also the longest line of code i've ever writen.
 function logData(data){
     for(i=0; i<data.people.length; i++) {
 
-        $('#container').children().append('<li><div class="person"><p class="person-name">Name: ' + data.people[i].name + '</p><p class="person-location">City: ' + data.people[i].location + '</p><p class="person-animal">Spirit Animal: ' + data.people[i].animal + '</p></div></li>');
+        $('#container').children().append('<li><div class="person" id="' + i + '"><p class="person-name">Name: ' + data.people[i].name + '</p><p class="person-location">City: ' + data.people[i].location + '</p><p class="person-animal">Spirit Animal: ' + data.people[i].animal + '</p></div></li>');
 
     }
+}
+
+//Below is the interval timer to move the carousel effect.
+var intervalTimer = function(){
+    setInterval(function(){
+
+        //This one would cause a slide out left effect. Not what the "client" specified upon further inspection of the directions.
+        //$("#container ul").animate({marginLeft:-480},1000,function(){
+        //    $(this).find("li:last").after($(this).find("li:first"));
+        //    $(this).css({marginLeft:0});
+        //})
+
+        //This is the fade out - change - fade in interval, looping back to the start.
+        $('#container ul').fadeOut(500,function(){
+            $(this).find("li:last").after($(this).find("li:first"))
+        });
+        $('#container ul').fadeIn(500,function(){
+        });
+//10 second interval
+    },10000);
+};
+
+//function for what happens when you click "previous"
+function clickPrevious(){
+    $('#container ul').fadeOut(500,function(){
+        $(this).find("li:first").before($(this).find("li:last"))
+    });
+    $('#container ul').fadeIn(500,function(){
+    });
+    resetTimer();
+}
+
+//function for what happens when you click "next"
+function clickNext(){
+    $('#container ul').fadeOut(500,function(){
+        $(this).find("li:last").after($(this).find("li:first"))
+    });
+    $('#container ul').fadeIn(500,function(){
+    });
+    resetTimer();
+}
+
+//function to reset the timers on click
+function resetTimer(){
+    clearInterval(intervalTimer);
 }
 
